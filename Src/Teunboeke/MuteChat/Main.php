1efb4b12740ec58166caa49f44a5e3387ae62c7c
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-21 11:52:54 +0100

    Update Main.php

--- Src/Teunboeke/MuteChat/Main.php
+++ Src/Teunboeke/MuteChat/Main.php
@@ -1,1 +1,15 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Teunboeke\MuteChat;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use function str_repeat;
+
+class MuteChat extends PluginBase implements Listener{
